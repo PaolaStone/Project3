ec82a49bece6878e9892ff76836a353ac9c4986a
Paola Stone <paola.stonemartinez@dell.com>
2019-11-30 00:15:39 -0500

    table shows but needs work

--- lab_discovery/client/src/components/FabricsSearch/FabricsSearch.js
+++ lab_discovery/client/src/components/FabricsSearch/FabricsSearch.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import { Table } from 'reactstrap';
 import SearchBy from '../Search';
-// import TableRow from '../TableRow';
-// import axios from 'axios'
-// import PORTS from '../../utils/PORTS';
 
-
-
-
-// class FabricsSearch extends React.Component {
 
 export default class FabricsSearch extends React.Component {
     
@@ -29,101 +22,54 @@ export default class FabricsSearch extends React.Component {
         console.log(response.status)
         this.setState({ports: data.allPorts, loading: false})
 
-        //this works//
-        // fetch('/api/ports',{ mode: 'cors'})
-        //     .then(function(response) {
-        //         return response.json();
-        //     }).then(function(response) {
-        //         console.log("responds  " + JSON.stringify(response) )
-                
-        //     })
-
-        //This works
     }
     
-   render() {
-       return (
-           <div>
-               {this.state.loading || !this.state.ports ? (
-               <div>loading</div> 
-               ) : (
-               <div>
-                   <table>
-                        {this.state.ports.map((port,i) => (
-                            <tr key={port.ID}>
-                                <td> {port.ID}</td>
-                                <td> {port.SwitchName}</td>
-                                <td> {port.Port}</td>
-                                <td> {port.Status}</td>
-                            </tr>
-                       ))}
-                   </table>
-                   <br/>
-                   {this.state.ports[0].ID}
-                   {this.state.ports[1].ID}
-               </div>) 
-                  
-               }
-           </div>
-       )
-   }
+    render() {
+        return (
+            <div>
+                
+                <div className="container-fluid">
+                {this.state.loading || !this.state.ports ? (
+                <div>loading</div> 
+                ) : (
+                    <div>
+                        <Table striped>
+                            <thead>
+                                <tr>
+                                    <th>Switch </th>
+                                    <th>Port</th>
+                                    <th>Status</th>
+                                    <th>System Name and Port</th>
+                                    <th>System www</th>
+                                    <th>Air Rack</th>
+                                    <th>Air Port</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.state.ports.map((port,i) => (
+                                <tr key={port.ID}>
+                                    <td> {port.SwitchName}</td>
+                                    <td> {port.Port}</td>
+                                    <td> {port.Status}</td>
+                                    <td> {port.SystemName_Port}</td>
+                                    <td> {port.SystemWWN}</td>
+                                    <td> {port.AirRackName}</td>
+                                    <td> {port.AirRackPort}</td>
+
+                                </tr>
+                                ))}
+                            </tbody>
+                        </Table>
+                        
+                    </div>
+                )}
+                </div>
+            </div>
+        )
+    }
 }
 
 
-
-//    componentDidMount() {
-        
-//         fetch('/api/ports')
-        
-//         .then(res => {
-//             console.log("this is res " + res.allPorts[0]);
-//             return res.json()
-            
-//         })
-//         .then(ports => {
-            
-//             this.setState({ports})
-//             console.log("this is ports " + ports);
-//         })
-//     }
-    // state = {
-    //    ports: [],
-    //    SwitchName: "",
-    //    Port: "",
-    //    Status: "",
-    //    SystemName_Port: "",
-    //    SystemWWN: "",
-    //    AirRackName: "",
-    //    AirRackPort: ""
-    // }
-
-    // componentDidMount() {
-
-        // this.loadPorts();
-
-    // }
-
-    // loadPorts = () => {
-    //     PORTS.getPorts()
-        
-    //         .then(res => 
-    //            this.setState({
-    //                 ports: res.data,
-    //                 SwitchName: "",
-    //                 Port: "",
-    //                 Status: "",
-    //                 SystemName_Port: "",
-    //                 SystemWWN: "",
-    //                 AirRackName: "",
-    //                 AirRackPort: ""
-    //             })
-                
-    //             )
-                
-    //         .catch(err => console.log(err))
-    //         console.log("this is ports" + this.state.ports)
-    // }
-    
 
     // render() {
     //     return (
