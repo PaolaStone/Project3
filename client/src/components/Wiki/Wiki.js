2cae1f33fd0844d419d6ae77802d681fe320a514
Paola Stone <paola.stonemartinez@dell.com>
2019-12-11 01:42:59 -0500

    fixed image size

--- client/src/components/Wiki/Wiki.js
+++ client/src/components/Wiki/Wiki.js
@@ -6,14 +6,14 @@ class Wiki extends React.Component {
       
       return (
         <div className="row">
-          <Col xs="3"></Col>
-          <Col xs="6">
+          <Col xs="2"></Col>
+          <Col xs="8">
             <img src=
-              "https://res.cloudinary.com/paolastone/image/upload/v1576044584/lab-discovery/wiki_zorcx2.png"
+              "https://res.cloudinary.com/paolastone/image/upload/v1576046365/lab-discovery/wiki_hpdmbd.png"
               alt="wiki"
             />
           </Col>
-         <Col xs="3"></Col>
+         <Col xs="1"></Col>
         </div>
 
       )
